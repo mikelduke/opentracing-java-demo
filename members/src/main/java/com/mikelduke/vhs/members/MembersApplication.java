9c7d367434a8b572b3bdea8b751968dac3eaf76e
mikel duke <mikelduke@gmail.com>
2018-08-04 11:53:42 -0500

    use starter that auto adds interceptor instead

--- members/src/main/java/com/mikelduke/vhs/members/MembersApplication.java
+++ members/src/main/java/com/mikelduke/vhs/members/MembersApplication.java
@@ -1,22 +1,18 @@
 package com.mikelduke.vhs.members;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.client.RestTemplate;
 
 import io.opentracing.Tracer;
-import io.opentracing.contrib.spring.web.client.TracingRestTemplateInterceptor;
 
 @SpringBootApplication
 public class MembersApplication {
-
-	@Autowired
-	Tracer tracer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MembersApplication.class, args);
@@ -25,8 +21,6 @@ public class MembersApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		RestTemplate template = builder.build();
-		template.setInterceptors(Collections.singletonList(new TracingRestTemplateInterceptor(tracer)));
-
 		return template;
 	}
 }
