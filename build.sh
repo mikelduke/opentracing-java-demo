60d90f387fc2e5ae29279ee170007228c18d0780
mikel duke <mikelduke@gmail.com>
2018-08-04 11:50:36 -0500

    add dockerfiles

--- build.sh
+++ build.sh
@@ -3,10 +3,23 @@
 echo 'building members'
 cd members
 ./gradlew clean build
+cd ..
 
 echo 'building vhs-catalog'
-cd ../vhs-catalog
+cd vhs-catalog
 ./gradlew clean build fatJar
+cd ..
+
+echo ''
+echo 'docker'
+echo 'building members'
+cd members
+docker build -t members .
+cd ..
+
+echo 'building vhs-catalog'
+cd vhs-catalog
+docker build -t vhs-catalog .
 
 echo ''
 echo 'done'
