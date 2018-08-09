cb8e0c925e18aa201aaf0a8c7de5f6ef63ee46ae
mikel duke <mikelduke@gmail.com>
2018-08-08 20:59:26 -0500

    remove unused imports

--- members/src/main/java/com/mikelduke/vhs/members/MembersApplication.java
+++ members/src/main/java/com/mikelduke/vhs/members/MembersApplication.java
@@ -1,15 +1,10 @@
 package com.mikelduke.vhs.members;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.client.RestTemplate;
-
-import io.opentracing.Tracer;
 
 @SpringBootApplication
 public class MembersApplication {
